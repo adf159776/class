de3501184595859f55c899d88aeeeaa53d22778b
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-06-07 21:47:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from user import Users,User,Login
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from flask_apispec.extension import FlaskApiSpec
-from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 api = Api(app)
