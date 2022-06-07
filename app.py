e5d86d3404aa2c5f91f4b1f6700b9f850abead14
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-06-08 02:13:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config.update({
         plugins=[MarshmallowPlugin()],
         openapi_version='2.0.0'
     ),
-    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
+    'APISPEC_SWAGGER_URL': '/',  # URI to access API Doc JSON
     'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
 })
 docs = FlaskApiSpec(app)
