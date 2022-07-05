c2623a1f89f0d90fc959407ca73464e8be3395e9
adf159776 <106952571+adf159776@users.noreply.github.com>
2022-07-05 16:14:49 +0800

    Create __init__.py

---
+++ FlaskApp/__init__.py
@@ -0,0 +1,59 @@
+from flask import Flask
+import pymysql
+import json
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
+
+app = Flask(__name__)
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Erp Project API',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
+    'APISPEC_SWAGGER_UI_URL': '/api', # URI to access UI of API Doc
+    'JSON_AS_ASCII': False
+})
+docs = FlaskApiSpec(app)
+
+def db_init():
+    db = pymysql.connect(
+        host='ec2-34-208-156-155.us-west-2.compute.amazonaws.com',
+        user='erp',
+        password='erp',
+        port=3306
+    )
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    return db, cursor
+
+@app.route('/employees/<fullname>/punch')
+def personpunch(fullname):
+    db, cursor = db_init()
+    cursor.execute(f"SELECT fullname,`inout`,FROM_UNIXTIME(timestamp, '%Y-%m-%d') date,FROM_UNIXTIME(timestamp, '%r') time,ipaddress FROM punch.`info` where fullname = '{fullname}' ORDER BY timestamp DESC;")
+    data = json.dumps(cursor.fetchall())
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return data
+
+@app.route('/employees/<fullname>/punch/<int:yearmonth>')
+def personpunchdate(fullname,yearmonth):
+    db, cursor = db_init()
+    cursor.execute(f"SELECT fullname,`inout`,FROM_UNIXTIME(timestamp, '%Y-%m-%d') date,FROM_UNIXTIME(timestamp, '%r') time,ipaddress FROM punch.`info` where fullname = '{fullname}' and FROM_UNIXTIME(timestamp, '%Y%m') = {yearmonth} ORDER BY timestamp DESC;")
+    data = json.dumps(cursor.fetchall())
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return data
+
+docs.register(personpunch)
+docs.register(personpunchdate)
+
+
+if __name__ == '__main__':
+    app.run()
